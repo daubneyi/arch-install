48d920899829c608bf3cdf5b6295f21e173a0aae
Ian Daubney <idaubney@gmail.com>
2020-12-28 13:53:28 +1100

    updated networking / added user
    updated networking added user configuration for YAY installation

--- sysdtemd-boot/arch-postinstall.sh
+++ sysdtemd-boot/arch-postinstall.sh
@@ -3,9 +3,10 @@
 wifiint=""
 wifissid=""
 wifipsk=""
+username=""
 
 usage() {                                      # Function: Print a help message.
-  echo "Usage: $0 [ -i WIFI_INT_NAME ] [ -s SSID ] [ -p PSK]" 1>&2 
+  echo "Usage: $0 [ -i WIFI_INT_NAME ] [ -s SSID ] [ -p PSK] [ -u USERNAME ]" 1>&2 
 }
 
 exit_abnormal() {                              # Function: Exit with error.
@@ -19,11 +20,12 @@ while getopts "i:s:p:" option; do
          i) wifiint=${OPTARG};;
          s) wifissid=${OPTARG};;
          p) wifipsk=${OPTARG};;
+         u) username=${OPTARG};;
 	 :) exit_abnormal;;
    esac
 done
 
-
+useradd --create-home --groups wheel --user-group --shell /usr/bin/fish ${username}
 
 systemctl enable --now iwd.service
 
@@ -33,16 +35,28 @@ echo "" >> /etc/systemd/network/25-wireless.network
 echo "[Network]" >> /etc/systemd/network/25-wireless.network
 echo "DHCP=yes" >> /etc/systemd/network/25-wireless.network
 
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+
+systemctl enable --now iwd.service
+systemctl enable --now sshd.service
+
 iwctl --passphrase $wifipsk station $wifiint connect $wifissid
 systemctl enable --now systemd-networkd.service
 systemctl enable --now systemd-resolved.service
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-git clone https://aur.archlinux.org/yay.git /root/yay/
-cd /root/yay/
-makepkg -si
-cd -
+pacman -S xorg
+pacman -S xf86-video-intel mesa
+
+Xorg :0 -configure
 
 pacman -S lightdm
+pacman -S lightdm
+
+su - ${username} <<SUEOF
+git clone https://aur.archlinux.org/yay.git $HOME/yay/
+cd $HOME/yay/
+makepkg -si
 
 yay lightdm-slick-greeter -S
+SUEOF
